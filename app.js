d08f57cd60c6abb609fb2d7012d114ad4e30de1d
Marcus W <marcuswassen@gmail.com>
2019-03-14 16:35:33 +0100

    added procfile

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 
 const app = express();
 
+const port = process.env.PORT || 5000
+
 app.get("/", (req, res) => {
   res.json({ user: "tobi" });
 });
 
-app.listen(4000, () => {
-  console.log("listening on port 4000");
+app.listen(port, () => {
+  console.log("listening on port " + port);
 });
