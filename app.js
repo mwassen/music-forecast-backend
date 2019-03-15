bf5f0015f7ed343642c007b2ebd18123c48bb0d9
Marcus W <marcuswassen@gmail.com>
2019-03-15 11:28:39 +0100

    added basic LF api functions

--- app.js
+++ app.js
@@ -18,12 +18,8 @@ const keys = {
 
 const port = process.env.PORT || 5000;
 
-/*
-/locations/
-/events/
-*/
-
-app.get("/location/:name", (req, res) => {
+// API call for location search bar
+app.get("/locations/:name", (req, res) => {
   const query = req.params.name;
 
   if (query !== "") {
@@ -47,6 +43,99 @@ app.get("/location/:name", (req, res) => {
       .then(data => {
         res.send(data);
       })
+  }
+});
+
+// API call for data retrieval
+app.get("/events/:locationid", (req, res) => {
+  const query = req.params.locationid;
+
+  const dataForViz = [];
+  const maxPages = 10;
+
+  fetchSongkick(query)
+  .then(data => {
+    // Handle multiple artist events
+    data.forEach(page => {
+      if (page.resultsPage) {
+        if (page.resultsPage.totalEntries === 0) return;
+        page.resultsPage.results.event.forEach(event => {
+          // Create data structure for event information
+          dataForViz.push({
+            date: event.start.date,
+            name: event.displayName,
+            artists: event.performance,
+            link: event.uri
+          });
+        });
+      }
+    });
+    const fetches = [];
+    dataForViz.forEach(event => {
+      event.artists.forEach(artist => {
+        fetches.push(fetchLastFm(artist));
+      });
+    });
+    return Promise.all(fetches);
+  })
+  .then(() => {
+    res.send(dataForViz.json())
+  });
+
+
+
+  function fetchSongkick(location) {
+    const songkickURL =
+      "https://api.songkick.com/api/3.0/metro_areas/" +
+      location +
+      "/calendar.json?apikey=" +
+      keys.songkick;
+
+    return baseFetch(1).then(data => {
+      const songkickReqs = [];
+      const totalEntries = data.resultsPage.totalEntries;
+      const reqPages =
+        totalEntries < maxPages * 50
+          ? Math.ceil(totalEntries / 50)
+          : maxPages;
+
+      songkickReqs.push(data);
+      for (let i = 2; i <= reqPages; i++) {
+        songkickReqs.push(baseFetch(i));
+      }
+      return Promise.all(songkickReqs);
+    });
+
+    function baseFetch(page) {
+      const pageParam = "&page=" + page;
+      return fetch(songkickURL + pageParam).then(response => {
+        if (response.status !== 200) {
+          res.status(200);
+          return;
+        } else return response.json();
+      });
+    }
+  }
+
+  function fetchLastFm(artist) {
+    const lastfmURL =
+      "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags&artist=" +
+      artist.displayName +
+      "&api_key=" +
+      keys.lastfm +
+      "&format=json";
+
+    return fetch(lastfmURL).then(response => {
+      if (response.status !== 200) {
+        res.status(200);
+        return;
+      } else
+        return response.json().then(data => {
+          if (data.toptags) {
+            artist.topGenres = data.toptags.tag;
+          }
+        });
+    });
   }
 });
 
