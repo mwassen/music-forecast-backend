5b68a3b23cbc34205c5ca2f55ee4578ea6f92751
Marcus W <marcuswassen@gmail.com>
2019-03-15 10:22:51 +0100

    basic location API added

--- app.js
+++ app.js
@@ -2,10 +2,40 @@ const express = require("express");
 
 const app = express();
 
+const keys = {
+  songkick: process.env.SK_KEY,
+  lastfm: process.env.LF_KEY
+}
+
 const port = process.env.PORT || 5000
 
-app.get("/", (req, res) => {
-  res.json({ user: "tobi" });
+/*
+/locations/
+/events/
+*/
+
+app.get("/location/:name", (req, res) => {
+  const query = req.params.name;
+
+  if (query !== "") {
+    const URL =
+      "https://api.songkick.com/api/3.0/search/locations.json?query=" +
+      query +
+      "&apikey=" +
+      keys.songkick;
+
+    fetch(URL)
+      .then(response => {
+        if (response.status !== 200) {
+          res.status(200)
+          // console.log(
+          //   "Looks like there was a problem with the SongKick location API. Status Code: " +
+          //     response.status
+          // );
+          return;
+        } else res.send(response);
+      })
+  }
 });
 
 app.listen(port, () => {
