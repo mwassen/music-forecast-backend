855fb4ba53bb2f4e2cf0d923ea42fa21382878d1
Marcus W <marcuswassen@gmail.com>
2019-03-15 10:54:14 +0100

    changed some settings CORS

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const fetch = require('node-fetch');
 
 const app = express();
 
-app.configure(function () {
-  app.use(allowCrossDomain);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+  next();
 });
 
 const keys = {
